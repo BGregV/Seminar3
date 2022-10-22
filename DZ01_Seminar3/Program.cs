9e2a777f72e041a231633d1c9ad5ebc423c401e0
BGregV <greguar@ninodom.ru>
2022-10-22 10:52:39 +0300

    Первое задание

---
+++ DZ01_Seminar3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int chislo = int.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+while (count <= chislo)
+{
+    /*double res = Math.Pow(count, 3);*/
+    int res = count*count*count;
+    Console.WriteLine($"{res} КУБ {count}");
+    count++;
+}
